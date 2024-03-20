bfff7d7971d1b573ed92e807aecaf94e1d325903
saraaah-qin <ya.qin@ucalgary.ca>
2024-03-20 17:14:25 -0600

    fixed updateRecipe function

--- src/main/java/com/prepper/prepper/controller/RecipesController.java
+++ src/main/java/com/prepper/prepper/controller/RecipesController.java
@@ -1,6 +1,7 @@
 package com.prepper.prepper.controller;
 
 import com.prepper.prepper.model.Recipes;
+import com.prepper.prepper.repository.RecipesRepository;
 import com.prepper.prepper.service.RecipesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,9 @@ public class RecipesController {
 
     @Autowired
     private RecipesService recipeService;
+
+    @Autowired
+    private RecipesRepository recipesRepository;
 
     @GetMapping("/getRecipes/{userID}")
     public ResponseEntity<List<Recipes>> getRecipesByUser(@PathVariable Integer userID) {
@@ -63,9 +67,17 @@ public class RecipesController {
                 .body(recipeToDelete);
     }
 
-    @PutMapping("/updateRecipe")
-    public ResponseEntity<Recipes> updateRecipe(@RequestBody Recipes recipe) {
-        Recipes updatedRecipe = recipeService.updateRecipe(recipe);
+    @PutMapping("/updateRecipe/{recipeID}")
+    public ResponseEntity<Recipes> updateRecipe(@RequestBody Recipes recipe, @PathVariable("recipeID") Integer recipeID) {
+        Recipes update = recipesRepository.findByRecipeID(recipeID);
+        update.setTitle(recipe.getTitle());
+        update.setImage(recipe.getImage());
+        update.setMeasurements(recipe.getMeasurements());
+        update.setIngredients(recipe.getIngredients());
+        update.setInstructions(recipe.getInstructions());
+        update.setPrepTime(recipe.getPrepTime());
+        recipesRepository.save(update);
+        Recipes updatedRecipe = recipesRepository.findByRecipeID(recipeID);
         return ResponseEntity
                 .ok()
                 .body(updatedRecipe);
