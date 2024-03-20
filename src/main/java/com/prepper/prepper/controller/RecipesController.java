832c3f9c088b008ec771916fdbc264f70b054b40
Kulaway <nicholas.garcia1@ucalgary.ca>
2024-03-20 11:17:43 -0600

    added response entity to recipes

--- src/main/java/com/prepper/prepper/controller/RecipesController.java
+++ src/main/java/com/prepper/prepper/controller/RecipesController.java
@@ -3,6 +3,7 @@ package com.prepper.prepper.controller;
 import com.prepper.prepper.model.Recipes;
 import com.prepper.prepper.service.RecipesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,36 +16,51 @@ public class RecipesController {
     private RecipesService recipeService;
 
     @GetMapping("/getRecipes/{userID}")
-    public List<Recipes> getRecipesByUser(@PathVariable Integer userID) {
-        return recipeService.getRecipesByUser(userID);
+    public ResponseEntity<List<Recipes>> getRecipesByUser(@PathVariable Integer userID) {
+        List<Recipes> recipes = recipeService.getRecipesByUser(userID);
+        return ResponseEntity
+                .ok()
+                .body(recipes);
     }
 
     @GetMapping("/getPublicRecipes")
-    public List<Recipes> getPublicRecipes() {
-        return recipeService.getPublicRecipes();
+    public ResponseEntity<List<Recipes>> getPublicRecipes() {
+        List<Recipes> recipes = recipeService.getPublicRecipes();
+        return ResponseEntity
+                .ok()
+                .body(recipes);
     }
 
     @GetMapping("/getRecipe/{recipeID}")
-    public Recipes getRecipe(@PathVariable Integer recipeID) {
-
-        return recipeService.getRecipe(recipeID);
+    public ResponseEntity<Recipes> getRecipe(@PathVariable Integer recipeID) {
+        Recipes recipe = recipeService.getRecipe(recipeID);
+        return ResponseEntity
+                .ok()
+                .body(recipe);
     }
 
     @GetMapping("/getAllRecipes")
-    public List<Recipes> getAllRecipes() {
-        return recipeService.getAllRecipes();
+    public ResponseEntity<List<Recipes>> getAllRecipes() {
+        List<Recipes> recipes = recipeService.getAllRecipes();
+        return ResponseEntity
+                .ok()
+                .body(recipes);
     }
 
     @PostMapping("/addRecipe")
-    public void addRecipe(@RequestBody Recipes recipe) {
-
+    public ResponseEntity<Recipes> addRecipe(@RequestBody Recipes recipe) {
         Recipes newRecipe = recipeService.saveRecipe(recipe);
+        return ResponseEntity
+                .ok()
+                .body(newRecipe);
     }
 
     @DeleteMapping(path = "/deleteRecipe/{recipeID}")
-    public void removeRecipe(
-            @PathVariable("recipeID")Integer recipeId) {
-        recipeService.removeRecipe(recipeId);
+    public ResponseEntity<Recipes> removeRecipe(@PathVariable("recipeID")Integer recipeId) {
+        Recipes recipeToDelete = recipeService.removeRecipe(recipeId);
+        return ResponseEntity
+                .ok()
+                .body(recipeToDelete);
     }
 
     @PutMapping()
