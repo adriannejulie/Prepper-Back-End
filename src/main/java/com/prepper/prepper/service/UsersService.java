21e08fee47e56bd0011d43fc50fbc06d2b70699e
saraaah-qin <ya.qin@ucalgary.ca>
2024-03-25 13:52:51 -0600

    added documentations to service classes

--- src/main/java/com/prepper/prepper/service/UsersService.java
+++ src/main/java/com/prepper/prepper/service/UsersService.java
@@ -11,17 +11,42 @@ public class UsersService {
     @Autowired
     private UsersRepository usersRepository;
 
+    /**
+     *  Saves user into database
+     *
+     * @param user The user information to be saved
+     * @return Saved user
+     */
     public Users saveUser(Users user) {
         return usersRepository.save(user);
     }
+
+    /**
+     *  Retrieves user information given userID
+     *
+     * @param userID The userID
+     * @return Corresponding user if found
+     */
     public Users getUserById(String userID) {
         return usersRepository.findByUserID(Long.valueOf(userID));
-
     }
+
+    /**
+     *  Retrieves user information given email
+     *
+     * @param email The email
+     * @return Corresponding user if found
+     */
     public Users getAccountByEmailAddress(String email) {
         return usersRepository.findByEmail(email);
     }
 
+    /**
+     *  Remove user given user email
+     *
+     * @param email The email of user to be removed
+     * @return Referenced user
+     */
     public Users deleteUserByEmail(String email) {
         Users userToDelete = usersRepository.findByEmail(email);
     
