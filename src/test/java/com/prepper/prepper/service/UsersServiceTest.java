4e22cbc6fcf3c1c34cdd553236a3e2916b28c8e9
adriannejulie <adriannejulia.lat@ucalgary.ca>
2024-03-24 23:01:43 -0600

    Added UserService Tests

--- src/test/java/com/prepper/prepper/service/UsersServiceTest.java
+++ src/test/java/com/prepper/prepper/service/UsersServiceTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 public class UsersServiceTest {
@@ -40,20 +41,41 @@ public class UsersServiceTest {
         Users savedUser = usersService.saveUser(newUser);
         assertNotNull(savedUser);
     }
+
     @Test
     public void getUserByIdTest(){
-        Users expectedUser = usersRepository.findByEmail("test1@gmail.com");
-        String actualUserID = expectedUser.getEmail();
+        Users expectedUser = new Users("test@gmail.com", "firstNameTest", "lastNameTest", "password", false);
+        expectedUser.setUserID(1L);
+        when(usersRepository.findByUserID(1L)).thenReturn(expectedUser);
 
-        //UserID is not available
-
+        Users actualUsers = usersService.getUserById(expectedUser.getUserID().toString());
         assertNotNull(expectedUser.getUserID());
-       // assertNotNull(actualUserID);
-        assertEquals(expectedUser.getUserID(), "test1@example.com");
-
-        //when(usersRepository.findByUserID(expectedUser.getUserID())).thenReturn(expectedUser);
-        //Users actualUser = usersService.getUserById(Long.toString(actualUserID));
+        assertEquals(expectedUser, actualUsers);
+        assertEquals(expectedUser.getUserID(), actualUsers.getUserID());
     }
+
+    @Test
+    public void getUserByEmailTest(){
+        Users expectedUser = new Users("test1@example.com", "firstName1", "lastName1", "password1", false);
+        when(usersRepository.findByEmail(expectedUser.getEmail())).thenReturn(expectedUser);
+
+        Users actualUsers = usersService.getAccountByEmailAddress(expectedUser.getEmail());
+        assertNotNull(actualUsers);
+        assertEquals(expectedUser, actualUsers);
+        assertEquals(expectedUser.getEmail(), actualUsers.getEmail());
+    }
+
+    @Test
+    public void deleteUserByEmailTest() {
+        Users user = usersRepository.findByEmail("test1@example.com");
+        assertNotNull(user);
+    
+        usersService.deleteUserByEmail("test1@example.com");
+        Users deletedUser = usersRepository.findByEmail("test1@example.com");
+    
+        assertNull(deletedUser); 
+    }
+    
 
 
     
